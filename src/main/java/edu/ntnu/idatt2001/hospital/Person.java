b61121b3ff4d47fc44ec0d65e04c58e82a120660
Ask <askrosand@gmail.com>
2021-02-20 16:55:00 +0100

    Implemented class Person according to the requirements.

---
+++ src/main/java/edu/ntnu/idatt2001/hospital/Person.java
@@ -0,0 +1,46 @@
+package edu.ntnu.idatt2001.hospital;
+
+public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    public Person(String firstName, String lastName, String socialSecurityNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    @Override
+    public String toString() {
+        return getFullName() + " (" + socialSecurityNumber + ")";
+    }
+}
