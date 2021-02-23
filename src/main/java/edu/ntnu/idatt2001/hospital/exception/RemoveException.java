945c73101c676d671a34f368f1f899fbccdc5779
Ask <askrosand@gmail.com>
2021-02-23 16:21:00 +0100

    Implemented class RemoveException according to the requirements.

---
+++ src/main/java/edu/ntnu/idatt2001/hospital/exception/RemoveException.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idatt2001.hospital.exception;
+
+public class RemoveException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    RemoveException(String message) {
+        super(message);
+    }
+
+}
